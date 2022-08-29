d1e1ca757692999340086395b406ceac33c867f9
sachiihaxor <106025611+sachiihaxor@users.noreply.github.com>
2022-08-29 17:41:43 +0700

    Create readme.php

---
+++ readme.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>   
+<title>Wh00ps! - Stoupid!</title>   
+<meta name='description' content='I am About Fucking!'>   
+<link rel='icon' href='https://i.pinimg.com/736x/74/e0/b5/74e0b5601b3c7c55da9531d9d5aa5748.jpg'>   
+<body bgcolor="white" oncontextmenu="return false" onkeydown="return false">   
+<style>   
+body{cursor:url("http:///foro.elhacker.net/elhacker.cur"),auto;}html{display:table;height:100%;width:100%;}body{display:table-row;}body{display:table-cell;vertical-align:middle;text-align:center;}a:link{text-decoration:none;}   
+   
+@import url('https://fonts.googleapis.com/css2?family=Russo+One&display=swap');   
+</style>   
+<link href="https://fonts.googleapis.com/css?family=Iceland" rel='stylesheet' type='text/css'>   
+<link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">   
+<br>   
+<img src='https://i.pinimg.com/736x/74/e0/b5/74e0b5601b3c7c55da9531d9d5aa5748.jpg' width="300">   
+<br>   
+<font face="Russo One" style="color:#000;text-shadow:0px 1px 5px #000;font-size:55px">Wh00ps! - Stoupid!   
+   <br>
+   <a href="https://hacker-h.org/"><font face="Iceland" style="color:#000;text-shadow:0px 1px 1px #000;font-size:20px">https://hacker-h.org</font></a> 
+   
+<font face="Iceland" style="color:#000;text-shadow:0px 1px 1px #000;font-size:20px">   
+<br>   
+ <font color="red">Wh00ps! - Stoupid!</font> - Mc'Sl0vv - Ateng337 - ZakSec166 - ./Sachii'Xploit - FellGans - Ardav
+<br>   
+<font face="Iceland" style="color:#000;text-shadow:0px 1px 5px #000;font-size:15px">./since 2016</font>   
+<br>   
+<video controls="" autoplay="" name="media"><source src="https://kodesumber.my.id/heiwkwk.mp3" type="audio/mpeg"></video><br><br><br> 
+</body> 
+</html>
